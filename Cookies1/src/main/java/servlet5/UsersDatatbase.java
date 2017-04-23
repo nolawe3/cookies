ad6ee592b9b716aca5cc16e08934aa0d1873ed66
Nolawe <nolawew103@gmail.com>
2017-04-23 14:50:36 -0500

    default2

--- Cookies1/src/main/java/servlet5/UsersDatatbase.java
+++ Cookies1/src/main/java/servlet5/UsersDatatbase.java
@@ -1,7 +1,7 @@
 package servlet5;
 
 import java.util.LinkedHashMap;
-//this is it Change to Defualt
+//this is it Change to Defualt2
 public class UsersDatatbase {
 	private User user;
 	private LinkedHashMap<String, String> userDB = new LinkedHashMap();
